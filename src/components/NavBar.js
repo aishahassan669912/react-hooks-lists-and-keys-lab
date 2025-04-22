ca1ba0f4d69f40302a06676ab0dc0f7148d122b2
Aisha Hassan <aishahassan669912@gmail.com>
2025-04-22 04:55:51 -0700

    phase-2/react-hooks-lists-and-keys-lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+  const links = [
+    { id: 1, name: "home", href: "#home" },
+    { id: 2, name: "about", href: "#about" },
+    { id: 3, name: "projects", href: "#projects" }
+  ];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map((link) => (
+        <a key={link.id} href={link.href}>
+          {link.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
 export default NavBar;
